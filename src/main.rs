b80b8bc4d5b7bb2b399eb00657ba5e39c9fe6449
Yan Mingzhi <realraindust@gmail.com>
2021-11-03 19:35:28 +0800

    add hex encode

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,11 @@ fn main() {
     let mut seed = [0u8; TEA_ID_LEN];
     OsRng.fill_bytes(&mut seed);
 
-    print!("{}", base64::encode(seed.to_vec()));
+    println!("Seed hex encoded (input in wallet when planting seed): ");
+    println!("0x{}", hex::encode(seed.to_vec()));
+
+    println!();
+
+    println!("Seed base64 encoded (input in your server when setting machine id): ");
+    println!("{}", base64::encode(seed.to_vec()));
 }
