921fb75cfff097bf48c8864d86ea567fd7a4d469
Yan Mingzhi <realraindust@gmail.com>
2021-11-03 18:51:48 +0800

    generate mock tea id use rand crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use rand::rngs::OsRng;
+use rand::RngCore;
+
+const TEA_ID_LEN: usize = 32;
+
 fn main() {
-    println!("Hello, world!");
+    let mut seed = [0u8; TEA_ID_LEN];
+    OsRng.fill_bytes(&mut seed);
+
+    print!("{}", base64::encode(seed.to_vec()));
 }
